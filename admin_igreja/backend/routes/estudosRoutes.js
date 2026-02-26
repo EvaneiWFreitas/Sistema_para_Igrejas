690fa7930d11374b6da91c210f3ce6e4ec4acf1d
prwilliam <nwillian533@gmail.com>
2026-02-26 14:40:02 -0300

    BackEnd - Criando a  Rota Get e GetById para o Estudo Bíblico

--- admin_igreja/backend/routes/estudosRoutes.js
+++ admin_igreja/backend/routes/estudosRoutes.js
@@ -5,6 +5,20 @@ const mongoose = require("mongoose");
 
 const Estudo = require("../models/Estudo");
 
+
+//**** MÉTODO PARA LISTAR  TODOS OS ESTUDOS CADASTRADOS ***//
+     //ROTA GET           /**( READ ) */
+/********************************************************* */
+
+router.get('/', async(req, res)=>{
+   const estudosList = await Estudo.find();
+   if(!estudosList){
+        res.status(500).json({success: false});
+   }
+   res.status(200).send(estudosList);
+});
+
+
 //*************** MÉTODO PARA SALVAR OS  ESTUDOS BÍBLICOS **************//
     //POST             /**( CREATE) */
 /*********************************************************** */
@@ -21,5 +35,10 @@ router.post("/", async (req, res) => {
     res.send(estudo);
 
 });
+
+
+
+
+
 
 module.exports = router;
