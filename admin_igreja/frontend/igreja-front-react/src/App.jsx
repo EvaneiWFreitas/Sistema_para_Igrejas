1da90467e73238cfce430ad2626b598596af51bc
prwilliam <nwillian533@gmail.com>
2026-02-26 09:27:32 -0300

    Criando o projeto - frontend da Igreja

---
+++ admin_igreja/frontend/igreja-front-react/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import HelloWorld from './HelloWorld'
+
+function App() {
+
+  return (
+    <>
+      <h1>Curso Sistema para Igreja.</h1>
+      <HelloWorld/>
+    </>
+  )
+}
+
+export default App
---
+++ admin_igreja/frontend/igreja-front-react/src/HelloWorld.jsx
@@ -0,0 +1,4 @@
+function HelloWorld(){
+    return <h1>OLá React - Dev Evanei Freitas.</h1>
+}
+export default HelloWorld
